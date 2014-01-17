b135cfb1f2e31fb75ba93ee9eefb55c574589f1e
SeungyoupBaek <bsy8220@naver.com>
2014-01-17 11:19:52 +0900

    PolyLine 실습 완료했습니다.

--- TmapTest/ViewController.m
+++ TmapTest/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "TMapView.h"
+@interface ViewController () <UISearchBarDelegate>
 
 @end
 
@@ -17,6 +17,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    TMapView* _mapView;
+    
+    _mapView = [[TMapView alloc] initWithFrame:CGRectMake(10, 80, 300, 300)]; [_mapView setSKPMapApiKey:@"7b7b1456-6496-3c3e-ae92-3cf87fd15065"]; _mapView.clipsToBounds = YES;
+    [self.view addSubview:_mapView];
+    CLLocationCoordinate2D coord[5] = {
+        CLLocationCoordinate2DMake(37.460143, 126.914062),
+        CLLocationCoordinate2DMake(37.469139, 126.981869),
+        CLLocationCoordinate2DMake(37.437930, 126.989937),
+        CLLocationCoordinate2DMake(37.413255, 126.959038),
+        CLLocationCoordinate2DMake(37.426752, 126.913548)
+    };
+    
+    TMapPolygon *polygon = [[TMapPolygon alloc] init];
+    [polygon setLineColor:[UIColor redColor]];
+    
+    [polygon setPolygonAlpha:0];
+    [polygon setLineWidth:8.0];
+    
+    for (int i = 0; i < 5; i++) {
+        [polygon addPolygonPoint:[TMapPoint mapPointWithCoordinate:coord[i]]];
+    }
+    [_mapView addTMapPolygonID:@"관악산" Polygon:polygon];
+
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
